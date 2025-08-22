d1c0e98e5f7b66e49d2b0f861559899bcf6beb2a
8pills-ui <grpapair@gmail.com>
2025-08-22 18:44:40 +0300

    create structure project

---
+++ src/data/products.ts
@@ -0,0 +1,22 @@
+import type { Product } from "@/types/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        title: 'Футболка майшоп',
+        price: 599,
+        description: 'Базовая футболка из хлопка ',
+    },
+        {
+        id: 12,
+        title: 'Кепка майшоп',
+        price: 399,
+        description: 'кепочка крутая ',
+    },
+        {
+        id: 13,
+        title: 'Носки майшоп',
+        price: 199,
+        description: 'Теплые носки  ',
+    },
+]
